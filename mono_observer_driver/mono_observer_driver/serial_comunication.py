e46ed2a1d1e90ac8005112d0ca1afd68817e849b
AngelVelascoJr <davidvp.advp@gmail.com>
2025-01-02 00:39:42 -0600

    update message sended, baudrate and logger info

--- mono_observer_driver/mono_observer_driver/serial_comunication.py
+++ mono_observer_driver/mono_observer_driver/serial_comunication.py
@@ -12,32 +12,32 @@ class RobotNode(Node):
         super().__init__(node_name)
         self.__topic_sub = self.create_subscription(ServoCtrl, '/robot_message', self.__recieve_positions_clbk, 10)
         self.get_logger().info("Trying to connect to port")
-        self.ser = serial.Serial(
-            port='/dev/ttyUSB0',
-            baudrate=9600,
-            timeout=3)
-        self.get_logger().info(f"serial conection: {self.ser.is_open}")
+        self.ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=3)
+        self.get_logger().info(f"conected to {self.ser.port}, serial conection status: {self.ser.is_open}")
         self.get_logger().info("started listening")
 
     # recieve data from main pc as topic message
     def __recieve_positions_clbk(self, msg:ServoCtrl):  
         self.get_logger().info(f"{len(msg.angles)} data received")
-        self.__i = 0
+        i = 0
+        message = ''
         for data in msg.angles:
-            self.get_logger().info(f"{self.__i}.- {data}")
-            self.__i += 1
+            self.get_logger().info(f"{i}.- {data}")
+            i += 1
+            message += f'{data}'
+            if i < len(msg.angles):
+                message += ','
+        message += ''
+        self.ser.write((f"{message}E").encode())
+        self.get_logger().warning(f"message sended: {message}")
         # send data throughth port to driver
-
-
 
 def main(args=None):
     rclpy.init(args=args)
     robot_node = RobotNode('servo_node')
-    while True:
-        robot_node.get_logger().info(robot_node.ser.readline())        
-    #rclpy.spin(robot_node)
-    #robot_node.ser.close()
-    #rclpy.shutdown()
+    rclpy.spin(robot_node)
+    robot_node.ser.close()
+    rclpy.shutdown()
 
 if __name__ == "__main__":
     main()

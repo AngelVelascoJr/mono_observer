0967d73a82c153abfa6f3a0925a2b8efa426e73f
AngelVelascoJr <davidvp.advp@gmail.com>
2025-03-02 21:27:29 -0600

    update ik file to fix parsing error

--- mono_observer_bringup/mono_observer_bringup/src/inverse_kinematics.py
+++ mono_observer_bringup/mono_observer_bringup/src/inverse_kinematics.py
@@ -8,6 +8,12 @@ class PosToIK():
     def __init__(self):
         pass
 
+    def ToAngleInt(rad:float) -> int:
+        return int((rad*180)/math.pi)
+
+    def clamp(num:float,maxVal:float,minVal:float) -> float:
+        return max(minVal,min(maxVal,num))
+    
     # inverse kinematicks for a 3dof (1 rot in z and 2 rotations in x)
     def GetIK(pos:ServoCtrl) -> ServoCtrl:  # must be a ServoCtrl and return a ServoCtrl
         _i = 0
@@ -25,19 +31,25 @@ class PosToIK():
         _i = 0
         newServoControl = ServoCtrl()
 
-        #para el angulo 0_Theta_1
-        angle1 = math.atan2(pos.angles[1]/pos.angles[0])
-        newServoControl.angles.append(angle1)
+        try:
+            #para el angulo 0_Theta_1
+            angle1 = math.atan2(pos.angles[1],pos.angles[0])
+            newServoControl.angles.append(PosToIK.clamp(PosToIK.ToAngleInt(angle1),180,0))
 
-        #para el angulo 2_Theta_3
-        angle3 = math.acos((pow(pos.angles[0],2)+pow((pos.angles[2]-_h_0_1_z),2)-pow(_h_1_2,2)-pow(_h_2_p,2))/(2*_h_1_2*_h_2_p))
-        
-        #para el angulo 1_Theta_2
-        alpha = 180-angle3
-        _h_1_p = math.sqrt(pow(_h_1_2,2)+pow(_h_2_p,2)-2*_h_1_2*_h_2_p*math.cos(alpha))
-        angle2 = (math.atan2((pos.angles[2]-_h_0_1_z)/(pos.angles[0]-_h_0_1_x))) - (math.acos((-pow(_h_2_p,2)+pow(_h_1_2,2)+pow(_h_1_p,2))/(2*_h_1_2*_h_1_p)))
+            #para el angulo 2_Theta_3
+            angle3 = math.acos((pow(pos.angles[0],2)+pow((pos.angles[2]-_h_0_1_z),2)-pow(_h_1_2,2)-pow(_h_2_p,2))/(2*_h_1_2*_h_2_p))
+            
+            #para el angulo 1_Theta_2
+            alpha = 180-angle3
+            _h_1_p = math.sqrt(pow(_h_1_2,2)+pow(_h_2_p,2)-2*_h_1_2*_h_2_p*math.cos(alpha))
+            angle2 = (math.atan2((pos.angles[2]-_h_0_1_z),(pos.angles[0]-_h_0_1_x))) - (math.acos((-pow(_h_2_p,2)+pow(_h_1_2,2)+pow(_h_1_p,2))/(2*_h_1_2*_h_1_p)))
 
-        newServoControl.angles.append(angle2)
-        newServoControl.angles.append(angle3)
-
-        return newServoControl
+            newServoControl.angles.append(PosToIK.clamp(PosToIK.ToAngleInt(angle2),180,0))
+            newServoControl.angles.append(PosToIK.clamp(PosToIK.ToAngleInt(angle3),180,0))
+            return newServoControl
+        except Exception as e:
+            print(f"Error al realizar el calculo de {pos.angles[0]},{pos.angles[1]},{pos.angles[2]}: {e}")
+            newServoControl.angles.append(int(0))
+            newServoControl.angles.append(int(0))
+            newServoControl.angles.append(int(0))
+            return newServoControl
